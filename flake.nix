c4fe129f750cc3999f338ab7bf7ede63afb583ad
Dan Kenerson <contact@dankenerson.com>
2026-01-12 14:12:32 +0100

    Add MBP configuration

--- flake.nix
+++ flake.nix
@@ -10,13 +10,37 @@
     # San Francisco Fonts | Apple Fonts
     apple-fonts.url= "github:Lyndeno/apple-fonts.nix";
     nvf.url = "github:notashelf/nvf";
+    nixos-hardware.url = "github:nixos/nixos-hardware";
   };
 
-  outputs = { nixpkgs, home-manager, apple-fonts, nvf, ... }: {
+  outputs = { nixpkgs, home-manager, apple-fonts, nvf, nixos-hardware, ... }: {
     nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
 	  ./hosts/desktop/configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager = {
+	      users.dan = { pkgs, ... }: {
+	        imports = [
+		  nvf.homeManagerModules.default
+		  ./home/dan/home.nix
+		];
+	      };
+
+	      useGlobalPkgs = true;
+	      useUserPackages = true;
+	      backupFileExtension = "backup";
+	    };
+	  }
+      ];
+      specialArgs = { inherit apple-fonts; };
+    };
+    nixosConfigurations.mbp = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+	  ./hosts/mbp/configuration.nix
+          nixos-hardware.nixosModules.apple-t2
 	  home-manager.nixosModules.home-manager
 	  {
 	    home-manager = {

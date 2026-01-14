c077043a304c36a815fefb27e98c174c8cc3c6af
Dan Kenerson <contact@dankenerson.com>
2026-01-14 17:08:31 +0100

    New hyprland start command

--- hosts/mbp/configuration.nix
+++ hosts/mbp/configuration.nix
@@ -53,7 +53,7 @@
   services.greetd = {
     enable = true;
     settings.default_session = {
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
       user = "greeter";
     };
   };
